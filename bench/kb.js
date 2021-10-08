899a66b7ed6b36b3d96da4084f149d732be05cb5
⚫️ <jp@bultmann.eu>
2021-10-08 14:13:25 +0200

    add rough bench for slow datascript query

--- bench/kb.js
+++ bench/kb.js
@@ -11,17 +11,24 @@ import {
   UFOID,
 } from "../mod.js";
 
-const { nameId, lovesId, titlesId } = UFOID.namedCache();
+const { nameId, lastNameId, ageId, eyeColorId, lovesId, titlesId } =
+  UFOID.namedCache();
 
 globalInvariants({
   [nameId]: { isUnique: true },
+  [lastNameId]: { isUnique: true },
   [lovesId]: { isLink: true, isUnique: true },
   [titlesId]: {},
+  [eyeColorId]: { isUnique: true },
+  [ageId]: { isUnique: true },
 });
 
 const knightsNS = namespace({
   [id]: { ...types.ufoid },
   name: { id: nameId, ...types.shortstring },
+  lastName: { id: lastNameId, ...types.shortstring },
+  eyeColor: { id: eyeColorId, ...types.shortstring },
+  age: { id: ageId, ...types.shortstring },
   loves: { id: lovesId },
   lovedBy: { id: lovesId, isInverse: true },
   titles: { id: titlesId, ...types.shortstring },
@@ -118,6 +125,76 @@ function kbQuery(b, size) {
   b.stop();
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function kbDSQuery(b) {
+  // Add some data.
+
+  let peoplekb = new KB(new TribleSet(), new BlobCache());
+  for (let i = 1; i < 1250; i++) {
+    peoplekb = peoplekb.with(knightsNS, ([ivan]) => [
+      {
+        [id]: ivan,
+        name: "Ivan",
+        lastName: `IvanSon${i}`,
+        eyeColor: "blue",
+        age: getRandomInt(100),
+      },
+    ]);
+  }
+  for (let i = 1; i < 1250; i++) {
+    peoplekb = peoplekb.with(knightsNS, ([ivan]) => [
+      {
+        [id]: ivan,
+        name: "Ivan",
+        lastName: `IvanSon${i}`,
+        eyeColor: "green",
+        age: getRandomInt(100),
+      },
+    ]);
+  }
+  for (let i = 1; i < 10000; i++) {
+    peoplekb = peoplekb.with(knightsNS, ([bob]) => [
+      {
+        [id]: bob,
+        name: `${i}Bob`,
+        lastName: `${i}Smith`,
+        eyeColor: "green",
+        age: getRandomInt(100),
+      },
+    ]);
+  }
+  for (let i = 1; i < 10000; i++) {
+    peoplekb = peoplekb.with(knightsNS, ([bob]) => [
+      {
+        [id]: bob,
+        name: `${i}Bob`,
+        lastName: `${i}Smith`,
+        eyeColor: "blue",
+        age: getRandomInt(100),
+      },
+    ]);
+  }
+  b.start();
+
+  // Query some data.
+  const results = [
+    ...find(knightsNS, ({ age, lastName }) => [
+      peoplekb.where([
+        {
+          name: "Ivan",
+          eyeColor: "blue",
+          age,
+          lastName,
+        },
+      ]),
+    ]),
+  ];
+  b.stop();
+}
+
 bench({
   name: "kbWith1e4",
   runs: 3,
@@ -131,6 +208,14 @@ bench({
   runs: 3,
   func(b) {
     kbQuery(b, 1e5);
+  },
+});
+
+bench({
+  name: "kbDSQuery",
+  runs: 3,
+  func(b) {
+    kbDSQuery(b);
   },
 });
 
