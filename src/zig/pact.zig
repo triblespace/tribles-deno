54d8f943498714bd4990293a9933bc928e9cb683
⚫️ <jp@bultmann.eu>
2021-10-11 03:13:32 +0200

    more work on zip pact

--- src/zig/pact.zig
+++ src/zig/pact.zig
@@ -2,11 +2,17 @@ const std = @import("std");
 const assert = std.debug.assert;
 const expect = std.testing.expect;
 
+const allocator = std.heap.page_allocator;
+
 const ByteBitset = std.StaticBitSet(256);
 
-const Byte_LUT = [256]u8;
+const HASH_COUNT = 4;
+const KEY_LENGTH = 64;
 
-const hash_count = 4;
+const MAX_ATTEMPTS = 8;
+
+const Byte_LUT = [256]u8;
+const Hash_LUT = [256][HASH_COUNT]u8;
 
 fn random_choice(rng: *std.rand.Random, set: ByteBitset) ?u8 {
     if (set.count() == 0) return null;
@@ -59,14 +65,23 @@ fn generate_identity_LUT(lut: *Byte_LUT) void {
     }
 }
 
-fn generate_hash_luts(comptime rng: *std.rand.Random) [hash_count]Byte_LUT {
-    var luts: [hash_count]Byte_LUT = undefined;
+fn generate_hash_luts(comptime rng: *std.rand.Random) Hash_LUT {
+    var luts: [HASH_COUNT]Byte_LUT = undefined;
 
     generate_identity_LUT(&luts[0]);
     for (luts[1..]) |*lut, i| {
-        generate_rand_LUT(rng, luts[0..i], hash_count - 1, lut);
+        generate_rand_LUT(rng, luts[0..i], HASH_COUNT - 1, lut);
     }
-    return luts;
+
+    var hash_lut: Hash_LUT = undefined;
+
+    for(luts) |lut, i| {
+        for(lut) |h, j| {
+            hash_lut[j][i] = h;
+        }
+    }
+
+    return hash_lut;
 }
 
 fn generate_pearson_lut(comptime rng: *std.rand.Random) Byte_LUT {
@@ -97,8 +112,22 @@ fn generate_pearson_lut(comptime rng: *std.rand.Random) Byte_LUT {
 ///       │    refcount
 /// branch depth
 ///
-const PACTHeader = packed struct { depth: u8, refcount: u32 };
+const PACTNode = union(enum) { inner: *PACTInner, leaf: *PACTLeaf };
 
+const PACTHeader = struct {
+    const Self = @This();
+
+    branch_depth: u8,
+    refcount: u32,
+
+    pub fn toNode(self: *Self) PACTNode {
+        if (self.branch_depth == KEY_LENGTH) {
+            return .{ .leaf = @fieldParentPtr(PACTLeaf, "header", self) };
+        } else {
+            return .{ .inner = @fieldParentPtr(PACTInner, "header", self) };
+        }
+    }
+};
 ///   ╔════════════════════════╗
 ///   ║       PACT Inner       ║
 ///   ╚════════════════════════╝
@@ -124,7 +153,7 @@ const Bucket = packed struct { key: u8 = 0, ptr: u40 = 0 };
 const PACTInner = comptime blk: {
     @setEvalBranchQuota(1000000);
     var rand_state = std.rand.Xoroshiro128.init(0);
-    const hash_luts = generate_hash_luts(&rand_state.random); // TODO: Can we move this into the struct scope?
+    const hash_lut = generate_hash_lut(&rand_state.random);
     const rand_lut = generate_pearson_lut(&rand_state.random);
 
     break :blk struct {
@@ -133,11 +162,67 @@ const PACTInner = comptime blk: {
         /// Note that the initial []buckets size needs to be >= |hash_LUTS|.
         /// This is to ensure that resized can't result in other hash functions pointing to now
         /// invalid buckets with false positively matching keys.
-        bucket_mask: u8 = hash_count - 1,
-        keys: ByteBitset = ByteBitset.initEmpty(),
+        header: PACTHeader,
+        key: u8[KEY_LENGTH],
+        bucket_mask: u8 = HASH_COUNT - 1,
+        child_set: ByteBitset = ByteBitset.initEmpty(),
 
-        pub fn init(allocator: *std.mem.Allocator) Self {
-            return Self{};
+        pub fn init(branch_depth: u8, key: *u8[KEY_LENGTH]) *Self {
+        const raw = allocator.alloc(u8, @sizeOf(Self) + @sizeOf(Bucket) * HASH_COUNT) catch unreachable;
+        const new = @ptrCast(Self, raw);
+        new.* = Self{.header = PACTHeader{.branch_depth=branch_depth,
+                                          .refcount = 1}),
+                    .key = key.*};
+        for(new.bucketSlice()) |*bucket| {
+            bucket.* = Bucket{};
+        }
+        return new;
+        }
+
+        fn putBranch(self: *Self, key: u8, value: *PACTHeader) {
+            const buckets = self.bucketSlice();
+            var ptr = @intCast(u40, @ptrCast(value));
+            
+            var attempts: u8 = 0;
+            while(true) {
+            random = rand_lut[random ^ key];
+            const bucket_hashes = hash_lut[key];
+            inline for (bucket_hashes) |*hash| {
+                hash &= self.bucket_mask;
+            }
+            inline for (bucket_hashes) |bucket| {
+                if(buckets[bucket].key == key or buckets[bucket].ptr == 0) {
+                    buckets[bucket].key = key;
+                    buckets[bucket].ptr = ptr;
+                    return;
+                }
+                const free = true;
+                inline for (hash_lut[bucket.key]) |hash| {
+                    if(bucket == hash & self.bucket_mask) {
+                        free = false;
+                        break;
+                    }
+                }
+                if(free) {
+                    buckets[bucket].key = key;
+                    buckets[bucket].ptr = ptr;
+                    return;
+                }
+            }
+            const displaced = if(self.bucket_mask == 0xFF) 0 else bucket_hashes[random & HASH_COUNT-1];
+            const displaced_key = buckets[displaced].key;
+            const displaced_ptr = buckets[displaced].ptr;
+            buckets[displaced].key = key;
+            buckets[displaced].ptr = ptr;
+            key = displaced_key;
+            ptr = displaced_ptr;
+
+            if(self.bucket_mask != 0xFF) attempts += 1;
+            if(attempts == MAX_ATTEMPTS) {
+                attempts = 0;
+                self.grow();
+            }
+            }
         }
 
         fn bucketSlice(self: *Self) []Bucket {
@@ -148,9 +233,9 @@ const PACTInner = comptime blk: {
 
         pub fn put(self: *Self, key: u8) void {}
         pub fn get(self: *Self, key: u8) ?*PACTHeader {
-            if (self.keys.isSet(key)) {
-                inline for (hash_luts) |lut, i| {
-                    const hash = lut[key];
+            if (self.child_set.isSet(key)) {
+                const hashes = hash_lut[key];
+                inline for (hashes) |hash| {
                     const bucket = self.bucketSlice()[hash & self.bucket_mask];
                     if (bucket.key == key and bucket.ptr != 0) {
                         return @intToPtr(*PACTHeader, bucket.ptr);
@@ -162,21 +247,84 @@ const PACTInner = comptime blk: {
     };
 };
 
-const PACTNode = union(enum) { inner: *PACTInner, leaf: *PACTLeaf };
+///      ╔════════════════════════╗
+///      ║       PACT Leaf        ║
+///      ╚════════════════════════╝
+///
+///
+/// 1 byte   4 byte
+/// always 64   │                                    64 byte
+///     │       └─────┐                                 │
+///     └──────────┐  │                                 │
+///                │┌──┐┌──────────────────────────────────────────────────────────────┐
+///                ╻┌──┐┌──────────────────────────────────────────────────────────────┐┌─────────────
+///                ┃│  ││                             key                              ││  value...
+///                ╹└──┘└──────────────────────────────────────────────────────────────┘└─────────────
+///                │  │
+///         ┌──────┘ ┌┘
+///         │        │
+///         │    refcount
+///   branch depth
+const PACTLeaf = struct {
+    header: PACTHeader,
+    key: u8[KEY_LENGTH],
+    value: usize = 0,
 
-const PACTHeader = packed struct {
-    const Self = @This();
-
-    branch_depth: u8,
-    refcount: u32,
-
-    pub fn toNode(self: *Self) PACTNode {
-        if (self.branch_depth == 255) {
-            return .{ .leaf = @fieldParentPtr(PACTLeaf, "header", self) };
-        } else {
-            return .{ .inner = @fieldParentPtr(PACTInner, "header", self) };
-        }
+    pub fn init(key: *u8[KEY_LENGTH], value: usize) *Self {
+        const new = allocator.create(Self) catch unreachable;
+        new.* = Self{.header = PACTHeader{.branch_depth=KEY_LENGTH,
+                                          .refcount = 1}),
+                    .key = key.*,
+                    .value = value};
+                    return new;
     }
+
+    pub fn peek(self: *Self, depth: u8) ?u8 {
+        if (depth < KEY_LENGTH) return self.key[depth];
+        return null;
+    }
+
+    pub fn propose(self: *Self, depth: u8, result_set: *ByteBitset) void {
+        var set = ByteBitset.initEmpty();
+        set.set(self.key[depth]);
+        result_set.setIntersection(set);
+    }
+
+        pub fn get(self: *Self, depth: u8, key: u8) ?*PACTHeader {
+        if (depth < KEY_LENGTH and this.key[depth] == v) return &self.header;
+        return null;
+    }
+
+    pub fn put(self: *Self, depth: u8, key: *u8[KEY_LENGTH], value: usize) *PACTHeader {
+        while (depth < KEY_LENGTH and this.key[depth] != key[depth]) depth += 1;
+
+      if (depth == KEY_LENGTH) {
+        return &self.header;
+      }
+
+      const sibling = PACTLeaf.init(key, value);
+
+      const branchChildren = [];
+      const leftIndex = this.key[depth];
+      const rightIndex = key[depth];
+      branchChildren[leftIndex] = this;
+      branchChildren[rightIndex] = sibling;
+      const branchChildbits = emptySet();
+      setBit(branchChildbits, leftIndex);
+      setBit(branchChildbits, rightIndex);
+      const hash = hash_combine(this.hash, sibling.hash);
+
+      return new PACTNode(
+        this.key,
+        depth,
+        branchChildbits,
+        branchChildren,
+        hash,
+        2,
+        owner
+      );
+    }
+
 };
 
 test "put nothing -> get nothing" {
