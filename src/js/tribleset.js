a19020ceb327e03d4a7a4ef5b7864249a2fb156e
⚫️ <jp@bultmann.eu>
2022-07-14 22:59:59 +0200

    work on unified constraint interface

--- src/js/tribleset.js
+++ src/js/tribleset.js
@@ -4,84 +4,277 @@ import {
   emptyValueIdIdTriblePACT,
 } from "./pact.js";
 import {
-  A,
-  E,
-  equalId,
   scrambleAEV,
   scrambleAVE,
   scrambleEAV,
   scrambleEVA,
   scrambleVAE,
   scrambleVEA,
-  V,
-  V1,
-  V2,
-  zero,
 } from "./trible.js";
 import { setBit } from "./bitset.js";
 
 const inmemoryCosts = 1; //TODO estimate and change to microseconds.
 // TODO return both count and latency. Cost = min count * max latency;
 
+const stack_empty = 0;
+const stack_e = 0;
+const stack_a = 1;
+const stack_v = 2;
+const stack_ea = 3;
+const stack_ev = 4;
+const stack_ae = 5;
+const stack_av = 6;
+const stack_ve = 7;
+const stack_va = 8;
+const stack_eav = 9;
+const stack_eva = 10;
+const stack_aev = 11;
+const stack_ave = 12;
+const stack_vea = 13;
+const stack_vae = 14;
+
 class MemTribleConstraint {
-  constructor(variableTree) {
-    this.explorationStack = [variableTree];
-  }
-  toString() {
-    return `MemTribleConstraint{variables:${this.explorationStack[0].children.map(
-      (t) => t.variable
-    )}}`;
+  constructor(tribleSet, e, a, v) {
+    if (e === a || e === v || a == v) {
+      throw new Error('Triple variables must be uniqe. Use explicit equality when inner constraints are required.')
+    }
+
+    this.state = stack_empty;
+    this.eVar = e;
+    this.aVar = a;
+    this.vVar = v;
+    this.EAVCursor = PaddedCursor(tribleSet.EAV.cursor(), 32);
+    this.EVACursor = PaddedCursor(tribleSet.EVA.cursor(), 32);
+    this.AEVCursor = PaddedCursor(tribleSet.AEV.cursor(), 32);
+    this.AVECursor = PaddedCursor(tribleSet.AVE.cursor(), 32);
+    this.VEACursor = PaddedCursor(tribleSet.VEA.cursor(), 32);
+    this.VAECursor = PaddedCursor(tribleSet.VAE.cursor(), 32);
   }
 
-  dependencies(dependsOn) {
-    const variables = this.explorationStack[0].children.map((t) => t.variable);
-    for (const v of variables) {
-      for (const u of variables) {
-        if (v !== u) {
-          setBit(dependsOn, u, v * 8);
+  peekByte() {
+    switch(this.state) {
+      case stack_empty: throw "unreachable";
+
+      case stack_e: return this.eavCursor.peek();
+      case stack_a: return this.aevCursor.peek();
+      case stack_v: return this.veaCursor.peek();
+
+      case stack_ea: return this.eavCursor.peek();
+      case stack_ev: return this.evaCursor.peek();
+      case stack_ae: return this.aevCursor.peek();
+      case stack_av: return this.aveCursor.peek();
+      case stack_ve: return this.veaCursor.peek();
+      case stack_va: return this.vaeCursor.peek();
+
+      case stack_eav: return this.eavCursor.peek();
+      case stack_eva: return this.evaCursor.peek();
+      case stack_aev: return this.aevCursor.peek();
+      case stack_ave: return this.aveCursor.peek();
+      case stack_vea: return this.veaCursor.peek();
+      case stack_vae: return this.vaeCursor.peek();
+      }
+  }
+
+  proposeByte(bitset) {
+    switch(this.state) {
+      case stack_empty: throw "unreachable";
+
+      case stack_e: this.eavCursor.propose(bitset); return;
+      case stack_a: this.aevCursor.propose(bitset); return;
+      case stack_v: this.veaCursor.propose(bitset); return;
+
+      case stack_ea: this.eavCursor.propose(bitset); return;
+      case stack_ev: this.evaCursor.propose(bitset); return;
+      case stack_ae: this.aevCursor.propose(bitset); return;
+      case stack_av: this.aveCursor.propose(bitset); return;
+      case stack_ve: this.veaCursor.propose(bitset); return;
+      case stack_va: this.vaeCursor.propose(bitset); return;
+
+      case stack_eav: this.eavCursor.propose(bitset); return;
+      case stack_eva: this.evaCursor.propose(bitset); return;
+      case stack_aev: this.aevCursor.propose(bitset); return;
+      case stack_ave: this.aveCursor.propose(bitset); return;
+      case stack_vea: this.veaCursor.propose(bitset); return;
+      case stack_vae: this.vaeCursor.propose(bitset); return;
+    }
+  }
+
+  pushByte(byte) {
+    switch(this.state) {
+      case stack_empty: throw "unreachable";
+
+      case stack_e: this.eavCursor.push(byte); this.evaCursor.push(byte); return;
+      case stack_a: this.aevCursor.push(byte); this.aveCursor.push(byte); return;
+      case stack_v: this.veaCursor.push(byte); this.vaeCursor.push(byte); return;
+
+      case stack_ea: this.eavCursor.push(byte); return;
+      case stack_ev: this.evaCursor.push(byte); return;
+      case stack_ae: this.aevCursor.push(byte); return;
+      case stack_av: this.aveCursor.push(byte); return;
+      case stack_ve: this.veaCursor.push(byte); return;
+      case stack_va: this.vaeCursor.push(byte); return;
+
+      case stack_eav: this.eavCursor.push(byte); return;
+      case stack_eva: this.evaCursor.push(byte); return;
+      case stack_aev: this.aevCursor.push(byte); return;
+      case stack_ave: this.aveCursor.push(byte); return;
+      case stack_vea: this.veaCursor.push(byte); return;
+      case stack_vae: this.vaeCursor.push(byte); return;
+    }
+  }
+
+  popByte() {
+    switch(this.state) {
+      case stack_empty: throw "unreachable";
+
+      case stack_e: this.eavCursor.pop(); this.evaCursor.pop(); return;
+      case stack_a: this.aevCursor.pop(); this.aveCursor.pop(); return;
+      case stack_v: this.veaCursor.pop(); this.vaeCursor.pop(); return;
+
+      case stack_ea: this.eavCursor.pop(); return;
+      case stack_ev: this.evaCursor.pop(); return;
+      case stack_ae: this.aevCursor.pop(); return;
+      case stack_av: this.aveCursor.pop(); return;
+      case stack_ve: this.veaCursor.pop(); return;
+      case stack_va: this.vaeCursor.pop(); return;
+
+      case stack_eav: this.eavCursor.pop(); return;
+      case stack_eva: this.evaCursor.pop(); return;
+      case stack_aev: this.aevCursor.pop(); return;
+      case stack_ave: this.aveCursor.pop(); return;
+      case stack_vea: this.veaCursor.pop(); return;
+      case stack_vae: this.vaeCursor.pop(); return;
+    }
+  }
+
+  proposeVariable(bitset) {
+    bitset.unsetAll();
+
+    switch(this.state) {
+        case stack_empty: bitset.set(self.eVar); bitset.set(self.aVar); bitset.set(self.vVar); return;
+
+        case stack_e: bitset.set(self.aVar); bitset.set(self.vVar); return;
+        case stack_a: bitset.set(self.eVar); bitset.set(self.vVar); return;
+        case stack_v: bitset.set(self.eVar); bitset.set(self.aVar); return;
+
+        case stack_ea: bitset.set(self.vVar); return;
+        case stack_ev: bitset.set(self.aVar); return;
+        case stack_ae: bitset.set(self.vVar); return;
+        case stack_av: bitset.set(self.eVar); return;
+        case stack_ve: bitset.set(self.aVar); return;
+        case stack_va: bitset.set(self.eVar); return;
+
+        default: return;
+    }
+  }
+
+  pushVariable(variable) {
+      if(this.eVar === variable) {
+        switch(this.state) {
+          case stack_empty: this.state = stack_e; return;
+
+          case stack_a: this.state = stack_ae; return;
+          case stack_v: this.state = stack_ve; return;
+
+          case stack_av: this.state = stack_ave; return;
+          case stack_va: this.state = stack_vae; return;
+
+          default: throw "unreachable";
         }
       }
-    }
-  }
+      if(this.aVar === variable) {
+        switch(this.state) {
+          case stack_empty: this.state = stack_a; return;
 
-  bid(isUnblocked) {
-    const lastExplored =
-      this.explorationStack[this.explorationStack.length - 1];
-    let candidateVariable = null;
-    let candidateCosts = Number.MAX_VALUE;
-    for (const child of lastExplored.children) {
-      for (const cursor of child.cursors) {
-        const costs = cursor.segmentCount() * inmemoryCosts;
-        const variable = child.variable;
-        if (costs <= candidateCosts && isUnblocked(variable)) {
-          candidateVariable = variable;
-          candidateCosts = costs;
+          case stack_e: this.state = stack_ea; return;
+          case stack_v: this.state = stack_va; return;
+
+          case stack_ev: this.state = stack_eva; return;
+          case stack_ve: this.state = stack_vea; return;
+
+          default: throw "unreachable";
         }
       }
-    }
-    return [candidateVariable, candidateCosts];
-  }
+      if(this.vVar == variable) {
+        switch(this.state) {
+          case stack_empty: this.state = stack_v; return;
 
-  pop(variable) {
-    const lastExplored =
-      this.explorationStack[this.explorationStack.length - 1];
-    if (lastExplored.variable === variable) {
-      this.explorationStack.pop();
-    }
-  }
+          case stack_e: this.state = stack_ev; return;
+          case stack_a: this.state = stack_av; return;
 
-  push(variable, ascending = true) {
-    const lastExplored =
-      this.explorationStack[this.explorationStack.length - 1];
-    let nextExplored = null;
-    for (const child of lastExplored.children) {
-      if (child.variable === variable) {
-        nextExplored = child;
+          case stack_ea: this.state = stack_eav; return;
+          case stack_ae: this.state = stack_aev; return;
+
+          default: throw "unreachable"; return;
+        }
       }
-    }
-    if (nextExplored === null) return [];
-    this.explorationStack.push(nextExplored);
-    return nextExplored.cursors;
+
+  }
+
+  popVariable() {
+      switch(this.state) {
+          case stack_empty: throw "unreachable";
+
+          case stack_e: this.state = stack_empty; return;
+          case stack_a: this.state = stack_empty; return;
+          case stack_v: this.state = stack_empty; return;
+
+          case stack_ea: this.state = stack_e; return;
+          case stack_ev: this.state = stack_e; return;
+          case stack_ae: this.state = stack_a; return;
+          case stack_av: this.state = stack_a; return;
+          case stack_ve: this.state = stack_v; return;
+          case stack_va: this.state = stack_v; return;
+
+          case stack_eav: this.state = stack_ea; return;
+          case stack_eva: this.state = stack_ev; return;
+          case stack_aev: this.state = stack_ae; return;
+          case stack_ave: this.state = stack_av; return;
+          case stack_vea: this.state = stack_ve; return;
+          case stack_vae: this.state = stack_va; return;
+      }
+  }
+
+  countVariable(variable) {
+      if(this.eVar === variable) {
+        switch(self.state) {
+          case stack_empty: return self.eavCursor.segmentCount();
+
+          case stack_a: return self.aevCursor.segmentCount();
+          case stack_v: return self.veaCursor.segmentCount();
+
+          case stack_av: return self.aveCursor.segmentCount();
+          case stack_va: return self.vaeCursor.segmentCount();
+
+          default: throw "unreachable";
+        }
+      }
+      if(this.aVar === variable) {
+          switch(self.state) {
+              case stack_empty: return self.aevCursor.segmentCount();
+
+              case stack_e: return self.eavCursor.segmentCount();
+              case stack_v: return self.vaeCursor.segmentCount();
+
+              case stack_ev: return self.evaCursor.segmentCount();
+              case stack_ve: return self.veaCursor.segmentCount();
+
+              default: throw "unreachable";
+          }
+      }
+      if(self.vVar === variable) {
+          switch(self.state) {
+              case stack_empty: return self.veaCursor.segmentCount();
+
+              case stack_e: return self.evaCursor.segmentCount();
+              case stack_a: return self.aveCursor.segmentCount();
+
+              case stack_ea: self.eavCursor.segmentCount();
+              case stack_ae: self.aevCursor.segmentCount();
+
+              default: throw "unreachable";
+          }
+      }
   }
 }
 
@@ -92,10 +285,7 @@ function flush_trible_buffer(
   AEV,
   AVE,
   VEA,
-  VAE,
-  EisA,
-  EisV,
-  AisV
+  VAE
 ) {
   for (const trible of buffer) {
     EAV.put(scrambleEAV(trible));
@@ -115,25 +305,6 @@ function flush_trible_buffer(
   for (const trible of buffer) {
     VAE.put(scrambleVAE(trible));
   }
-  for (const trible of buffer) {
-    const e = E(trible);
-    const a = A(trible);
-    const v1 = V1(trible);
-    const v2 = V2(trible);
-    const eIsA = equalId(e, a);
-    const eIsV = zero(v1) && equalId(e, v2);
-    const aIsV = zero(v1) && equalId(a, v2);
-
-    if (eIsA) {
-      EisA.put(scrambleEAV(trible));
-    }
-    if (eIsV) {
-      EisV.put(scrambleEAV(trible));
-    }
-    if (aIsV) {
-      AisV.put(scrambleAEV(trible));
-    }
-  }
 }
 
 const BUFFER_SIZE = 64;
@@ -144,10 +315,7 @@ class TribleSet {
     AEV = emptyIdIdValueTriblePACT,
     AVE = emptyIdValueIdTriblePACT,
     VEA = emptyValueIdIdTriblePACT,
-    VAE = emptyValueIdIdTriblePACT,
-    EisA = emptyIdIdValueTriblePACT, // Same order as EAV
-    EisV = emptyIdIdValueTriblePACT, // Same order as EAV
-    AisV = emptyIdIdValueTriblePACT // Same order as AEV
+    VAE = emptyValueIdIdTriblePACT
   ) {
     this.EAV = EAV;
     this.EVA = EVA;
@@ -155,9 +323,6 @@ class TribleSet {
     this.AVE = AVE;
     this.VEA = VEA;
     this.VAE = VAE;
-    this.EisA = EisA;
-    this.EisV = EisV;
-    this.AisV = AisV;
   }
 
   with(tribles) {
@@ -167,9 +332,6 @@ class TribleSet {
     const AVE = this.AVE.batch();
     const VEA = this.VEA.batch();
     const VAE = this.VAE.batch();
-    const EisA = this.EisA.batch();
-    const EisV = this.EisV.batch();
-    const AisV = this.AisV.batch();
 
     const buffer = new Array(BUFFER_SIZE);
     buffer.length = 0;
@@ -183,15 +345,12 @@ class TribleSet {
           AEV,
           AVE,
           VEA,
-          VAE,
-          EisA,
-          EisV,
-          AisV
+          VAE
         );
         buffer.length = 0;
       }
     }
-    flush_trible_buffer(buffer, EAV, EVA, AEV, AVE, VEA, VAE, EisA, EisV, AisV);
+    flush_trible_buffer(buffer, EAV, EVA, AEV, AVE, VEA, VAE);
 
     return new TribleSet(
       EAV.complete(),
@@ -199,10 +358,7 @@ class TribleSet {
       AEV.complete(),
       AVE.complete(),
       VEA.complete(),
-      VAE.complete(),
-      EisA.complete(),
-      EisV.complete(),
-      AisV.complete()
+      VAE.complete()
     );
   }
 
@@ -215,139 +371,7 @@ class TribleSet {
   }
 
   constraint(e, a, v) {
-    const EAVCursor = this.EAV.cursor();
-    const EVACursor = this.EVA.cursor();
-    const AEVCursor = this.AEV.cursor();
-    const AVECursor = this.AVE.cursor();
-    const VEACursor = this.VEA.cursor();
-    const VAECursor = this.VAE.cursor();
-    const EisACursor = this.EisA.cursor();
-    const EisVCursor = this.EisV.cursor();
-    const AisVCursor = this.AisV.cursor();
-
-    if (e === a && e === v) {
-      return new MemTribleConstraint({
-        children: [{ variable: e, cursors: [EisACursor, EisVCursor] }],
-      });
-    }
-    if (e === a) {
-      return new MemTribleConstraint({
-        children: [
-          {
-            variable: e,
-            cursors: [EVACursor, EisACursor],
-            children: [{ variable: v, cursors: [EVACursor], children: [] }],
-          },
-          {
-            variable: v,
-            cursors: [VEACursor],
-            children: [
-              {
-                variable: e,
-                cursors: [VEACursor, EisACursor],
-                children: [],
-              },
-            ],
-          },
-        ],
-      });
-    }
-    if (e === v) {
-      return new MemTribleConstraint({
-        children: [
-          {
-            variable: e,
-            cursors: [EAVCursor, EisVCursor],
-            children: [{ variable: a, cursors: [EAVCursor], children: [] }],
-          },
-          {
-            variable: a,
-            cursors: [AEVCursor],
-            children: [
-              {
-                variable: e,
-                cursors: [AEVCursor, EisVCursor],
-                children: [],
-              },
-            ],
-          },
-        ],
-      });
-    }
-    if (a === v) {
-      return new MemTribleConstraint({
-        children: [
-          {
-            variable: e,
-            cursors: [EAVCursor],
-            children: [
-              {
-                variable: a,
-                cursors: [EAVCursor, AisVCursor],
-                children: [],
-              },
-            ],
-          },
-          {
-            variable: a,
-            cursors: [AEVCursor, AisVCursor],
-            children: [{ variable: e, cursors: [AEVCursor], children: [] }],
-          },
-        ],
-      });
-    }
-    return new MemTribleConstraint({
-      children: [
-        {
-          variable: e,
-          cursors: [EAVCursor, EVACursor],
-          children: [
-            {
-              variable: a,
-              cursors: [EAVCursor],
-              children: [{ variable: v, cursors: [EAVCursor], children: [] }],
-            },
-            {
-              variable: v,
-              cursors: [EVACursor],
-              children: [{ variable: a, cursors: [EVACursor], children: [] }],
-            },
-          ],
-        },
-        {
-          variable: a,
-          cursors: [AEVCursor, AVECursor],
-          children: [
-            {
-              variable: e,
-              cursors: [AEVCursor],
-              children: [{ variable: v, cursors: [AEVCursor], children: [] }],
-            },
-            {
-              variable: v,
-              cursors: [AVECursor],
-              children: [{ variable: e, cursors: [AVECursor], children: [] }],
-            },
-          ],
-        },
-        {
-          variable: v,
-          cursors: [VEACursor, VAECursor],
-          children: [
-            {
-              variable: e,
-              cursors: [VEACursor],
-              children: [{ variable: a, cursors: [VEACursor], children: [] }],
-            },
-            {
-              variable: a,
-              cursors: [VAECursor],
-              children: [{ variable: e, cursors: [VAECursor], children: [] }],
-            },
-          ],
-        },
-      ],
-    });
+    return new MemTribleConstraint(this, e, a, v);
   }
 
   count() {
