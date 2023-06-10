07bb133f734cb15d199313d398bdc2136e0e2f78
⚫️ <jp@bultmann.eu>
2023-06-10 15:18:24 +0200

    update wasm

--- src/js/wasm.js
+++ src/js/wasm.js
@@ -58,7 +58,7 @@ export function hash_equal(l, r) {
   return instance.exports.hash_equal() === 1;
 }
 
-// # Blake2b256
+// # Blake3
 const blake3_digest_size = 32;
 const blake_buffer_size = 16384;
 
@@ -88,60 +88,4 @@ export function blake3(data, out) {
   instance.exports.blake3_finish();
   out.set(_global_blake3_out);
   instance.exports.blake3_deinit();
-}
-
-// # Commits
-const serialize_header_size = 128;
-const serialize_max_trible_count = 1021;
-const trible_size = 64;
-const serialize_max_size = serialize_header_size +
-  (serialize_max_trible_count * trible_size);
-const serialize_secret_size = 32;
-
-const _global_serialize_secret = new Uint8Array(
-  instance.exports.memory.buffer,
-  instance.exports._global_serialize_secret,
-  serialize_secret_size,
-);
-const _global_serialize_buffer = new Uint8Array(
-  instance.exports.memory.buffer,
-  instance.exports._global_serialize_buffer,
-  serialize_max_size,
-);
-
-const _global_serialize_buffer_metaid = new Uint8Array(
-  instance.exports.memory.buffer,
-  instance.exports._global_serialize_buffer + 112,
-  16,
-);
-
-export function setMetaId(metaId) {
-  _global_serialize_buffer_metaid.set(metaId);
-}
-
-const _global_serialize_buffer_tribles = new Uint8Array(
-  instance.exports.memory.buffer,
-  instance.exports._global_serialize_buffer + 128,
-  serialize_max_trible_count * trible_size,
-);
-
-export function setTrible(i, trible) {
-  _global_serialize_buffer_tribles.subarray(
-    i * TRIBLE_SIZE,
-    (i + 1) * TRIBLE_SIZE,
-  ).set(trible);
-}
-
-export function verify(data) {
-  _global_serialize_buffer.set(data);
-  return instance.exports.verify(data.length);
-}
-
-export function sign(secret, trible_count) {
-  _global_serialize_secret.set(secret);
-  if (!instance.exports.sign(trible_count)) throw "Failed to sign tribles!";
-  return _global_serialize_buffer.subarray(
-    0,
-    _global_serialize_buffer_tribles + trible_count * trible_size,
-  );
 }
