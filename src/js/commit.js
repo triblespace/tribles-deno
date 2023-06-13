7a13df11c4cff9bdca3585ce540c6d4c111165e4
⚫️ <jp@bultmann.eu>
2023-06-13 11:04:55 +0200

    cleanup

--- src/js/commit.js
+++ src/js/commit.js
@@ -1,53 +1,6 @@
-import { TRIBLE_SIZE } from "./trible.js";
 import { types } from "./types.js";
 import { UFOID } from "./types/ufoid.js";
 import { id } from "./namespace.js";
-import { authNS } from "./auth.js";
-import {
-  deserialize as tribleDeserialize,
-  serialize as tribleSerialize,
-} from "./tribleset.js";
-import {
-  deserialize as blobDeserialize,
-  serialize as blobSerialize,
-} from "./blobcache.js";
-
-// Each commits starts with a 16 byte zero marker for framing.
-//
-//   Note that the use of nil/zero ids is invalid in tribles, which allows
-//   us to use it in this fashion without data transparency issues.
-//
-// This is followed by a 32 byte checksum, the verification algorithm of
-// which is stored as part of the commit data.
-//
-// The following commit id identifies an entity in the commit itself that
-// contains metadata about this commit, such as the checksum algorithm,
-//  creation time, additional information on provenance and the author,
-// and wether this commit is part of a larger unit of information.
-//
-//   The reason for the signature awkwardly crossing cache lines,
-//   with the commit id following it is that this allows implementations
-//   to conveniently sign the both the commit id and the payload without
-//   having to copy them into a contiguous buffer.
-//
-// The following payload consists of both the data and metadata trible
-// sorted in canonical EAV order.
-//
-//      16 byte                 32 byte                 16 byte
-//         │                       │                       │
-// ┌──────────────┐┌──────────────────────────────┐┌──────────────┐
-// ┌──────────────┐┌──────────────────────────────┐┌──────────────┐
-// │     zero     ││           checksum           ││  commit it   │
-// └──────────────┘└──────────────────────────────┘└──────────────┘
-//
-//                              64 byte
-//                                 │
-// ┌──────────────────────────────────────────────────────────────┐
-// ┌──────────────┬┬──────────────┬┬──────────────────────────────┐*
-// │    entity    ││  attribute   ││            value             │
-// └──────────────┴┴──────────────┴┴──────────────────────────────┘
-//                                 │
-//                              trible
 
 const udp_max_commit_size = 1021;
 
